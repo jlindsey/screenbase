940fc8351df6fc9bfe633c57a53263bf45b32e12
Josh Lindsey <joshua.s.lindsey@gmail.com>
2017-04-18 14:20:05 -0400

    adds __VERSION__ to package

--- screenbase/__init__.py
+++ screenbase/__init__.py
@@ -12,3 +12,9 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Screenbase package
+"""
+
+import pkg_resources
+__VERSION__ = pkg_resources.require(__name__)[0].version
